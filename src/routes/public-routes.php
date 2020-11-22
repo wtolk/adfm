119461a78174b64aadbb9474d440d4226fa9754d
Seryak <misha-seryak@ya.ru>
2020-11-22 17:56:36 +0700

    переместил роуты

--- src/routes/public-routes.php
+++ src/routes/public-routes.php
@@ -4,3 +4,4 @@ Route::group(['namespace' => 'App\Adfm\Controllers\Site'], function () {
     Route::get('/', 'PageController@showMainPage')->name('adfm.show.main-page');
     Route::get('/{slug}', 'PageController@showPage')->name('adfm.show.page');
 });
+
