c3d79682082b5f3e38477fc097aacfa958e7652e
Seryak <misha-seryak@ya.ru>
2020-11-22 18:25:57 +0700

    Дебаггер в хелперах

---
+++ src/Helpers/Dev.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Adfm\Helpers;
+
+class Dev
+{
+    static function dd(...$var)
+    {
+        unset(\Kint::$plugins[5], \Kint::$plugins[10], \Kint::$plugins[9]);
+//        dd(\Kint::$plugins);
+        d($var);
+        die;
+    }
+}
